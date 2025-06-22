e60975d626656836c9f5b97228f38f84673a025b
Pankaj Kumar <142082497+kumarpankaj3404@users.noreply.github.com>
2025-06-22 10:56:55 +0530

    Created routes to register user

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
-// require('dotenv').config({path: './env'});
-
-import dotenv from "dotenv";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import express from "express";
-
-const app = express();
-
+import {app} from './app.js'
 dotenv.config({
-    path: './env'
+    path: './.env'
 })
+
+
+
 connectDB()
-.then(()=> {
-    app.on("error",(error) =>{
-            console.log("ERR : ",error);
-            throw error;
-        })
-    app.listen(process.env.PORT || 8000 , ()=>{
-        console.log(`Server is running on PORT : ${process.env.PORT}`);
-        
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
-    console.log("Mongo Db connection failed !!",err);
-    
+    console.log("MONGO db connection failed !!! ", err);
 })
 
 
@@ -35,21 +28,24 @@ connectDB()
 
 
 /*
-    import express from "express";
-    const app = express();
-    ;( async() =>{
+import express from "express"
+const app = express()
+( async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        app.on("error",(error) =>{
-            console.log("ERR : ",error);
-            throw error;
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
         })
 
-        app.listen(process.env.PORT, ()=>{
-            console.log(`App is listening on port ${process.env.PORT}`)
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
         })
+
     } catch (error) {
-        console.error("Error :",error);
-        throw err;
+        console.error("ERROR: ", error)
+        throw err
     }
-})() */
+})()
+
+*/
